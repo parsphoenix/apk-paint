65c0eebf0897524e804f36b53d8b5a8fc06a2008
parsphoenix <dragonhost94@yahoo.com>
2026-06-29 16:21:12 +0330

    Track .output directory

---
+++ .output/server/_tanstack-start-manifest_v-NRJ_PqO9.mjs
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "c:/Users/Erfaneh/Desktop/paint/src/routes/__root.tsx", children: ["/", "/room/$code"], preloads: ["/assets/index-B_At5UJ1.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-B_At5UJ1.js" } }] } } });
+export {
+  tsrStartManifest
+};
